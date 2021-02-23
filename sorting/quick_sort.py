5239c173a17697cd7aeb86b8968cf20fff4462eb
Harshpreet Singh Johar <50266759+harshjohar@users.noreply.github.com>
2021-02-24 01:38:54 +0530

    small mistake

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -3,6 +3,7 @@ import random
 def partition(li, left, right):
     # You are given a list of size N, ans an element from the list called as'pivot element'.
     # You need to segeregate all the elements lesser than pivot on the left side & greater than pivot on the right side in any order.    m = left
+    m = left
     pivot_index = random.randrange(left, right)
     pivot = li[pivot_index]
     li[right], li[pivot_index] = li[pivot_index], li[right]
